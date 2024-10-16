26bb75a0d3c0943d94df03fbe08e94d7ff1d64e5
Sam Hoile <samuel.hoile@gmail.com>
2024-10-16 13:12:36 +0100

    fixed .env

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_SOCKET_BACKEND_URL;
-console.log("URL",SOCKET_URL)
+console.log("SOCKET_URL",SOCKET_URL)
 
-// export const socket = io(SOCKET_URL)
-export const socket = io("http://localhost:3001")
+export const socket = io(SOCKET_URL)
