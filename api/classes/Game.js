ac64390fe534ac4e0ede11c3f7f2bf1ba732a20a
Lucia_F <luciafernandez4e@gmail.com>
2024-10-16 14:23:56 +0100

    Added checkGuess method and resetGame method

--- api/classes/Game.js
+++ api/classes/Game.js
@@ -8,8 +8,33 @@ class Game {
         return Math.floor(Math.random() * 100) + 1;
     }
 
-    addPlayer(user) {
-        this.players.push(user);
+    addPlayer(player) {
+        this.players.push(player);
+    }
+
+    checkGuess() {
+        if (this.players.every(player => player.currentGuess !== null)) {
+            let closestPlayer = this.players[0];
+            let smallestDifference = Math.abs(this.players[0].currentGuess - this.targetNumber);
+
+            this.players.forEach(player => {
+                const difference = Math.abs(player.currentGuess - this.targetNumber);
+                if (difference < smallestDifference) {
+                    closestPlayer = player;
+                    smallestDifference = difference;
+                }
+            })
+            closestPlayer.wonRound();
+            this.resetGame();
+
+            return { success: true, closestPlayer};
+        }
+        return { success: false, message: 'Waiting for other players to submit guesses'}
+    }
+
+    resetGame() {
+        this.targetNumber = this.generateRandomNumber();
+        this.players.forEach(player => player.currentGuess = null);
     }
 }
 
