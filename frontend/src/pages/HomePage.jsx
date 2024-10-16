cc4c703ea4e908aa3c3ea2148625970131ced8a4
Joe Winter <joewinter333@gmail.com>
2024-10-16 13:30:17 +0100

    added create room button

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,25 +2,30 @@
 
 //imports needed
 import { Link } from "react-router-dom";
-{/*import { Button } from "../components/Button"; */}
+import { socket } from "../socket";
+import { Button } from "../components/Button";
 
 // page function
-export function HomePage() {
+export function HomePage({gameLink}) {
+
+  const handleClick = () => {
+    socket.emit("create_room");
+  };
   return (
     <>
-    <div className="home">
+      <div className="home">
         <h1 data-testid="game-name">Guess the number!</h1>
         <p>
-            Rules of the game: 
-            <ul>
-                <li>Guess the number between 1 and 100</li>
-                <li>2 to 6 players</li>
-            </ul>
+          Rules of the game:
+          <ul>
+            <li>Guess the number between 1 and 100</li>
+            <li>2 to 6 players</li>
+          </ul>
         </p>
         <Link to="/lobby" className="lobby-link"></Link>
-        {/* <Button onClick={handleClick}>Create game</Button> */}
-
-    </div>
+        <Button handleClick={handleClick} buttonText="Create Room"></Button>
+        {gameLink && <div>{gameLink}</div>}
+      </div>
     </>
   );
 }
