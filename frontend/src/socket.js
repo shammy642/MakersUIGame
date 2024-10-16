ec6059063259925bcea2b68c0516ea2c36d8a94c
Joe Winter <joewinter333@gmail.com>
2024-10-16 13:04:34 +0100

    env problems

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,4 +1,6 @@
 import { io } from "socket.io-client";
-const SOCKET_URL = import.meta.env.SOCKET_BACKEND_URL;
+const SOCKET_URL = import.meta.env.VITE_SOCKET_BACKEND_URL;
+console.log("URL",SOCKET_URL)
 
-export const socket = io(SOCKET_URL)
+// export const socket = io(SOCKET_URL)
+export const socket = io("http://localhost:3001")
