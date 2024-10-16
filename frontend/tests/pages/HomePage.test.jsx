ac53f67ede400051437dea0a8033647e409e1d2b
Joe Winter <joewinter333@gmail.com>
2024-10-16 11:17:00 +0100

    deleted empty test files

--- frontend/tests/pages/HomePage.test.jsx
+++
@@ -1,9 +0,0 @@
-// Tests for the homepage where the host can create a game
-
-//required imports
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
-import { HomePage } from "../../src/pages/HomePage";
-
-
-
--- frontend/tests/pages/RoundEnd.test.jsx
+++
@@ -1,7 +0,0 @@
-// Tests for the round end page where players can view the scoreboard, the number, the winner(s), 
-// and choose whether to go to the next round or quit the game
-
-//required imports
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
-import { RoundEnd } from "../../src/pages/RoundEnd";
