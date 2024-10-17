01f76d687d6cdfb30976fc52207697d36e5ec456
Sam Hoile <samuel.hoile@gmail.com>
2024-10-17 10:56:48 +0100

    separated socket from app. added PORT and CORS_ALLOWED_URL to backend env. Frontend ENV - removed SOCKET_URL. Now only VITE_BACKEND_URL

--- api/index.js
+++ api/index.js
@@ -1,13 +1,29 @@
 // docs: https://github.com/motdotla/dotenv#%EF%B8%8F-usage
 require("dotenv").config();
 
-const app = require("./app.js");
+const server = require("./app.js");
+const io = require('./socket.js')
+const crypto = require("crypto")
 
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`)
+  console.log(`Users Connected: ${io.engine.clientsCount}`)
 
+  socket.on('disconnect', () => {
+    console.log("User disconnected!")
+    console.log(`Users Connected: ${io.engine.clientsCount}`)
+  })
+
+  socket.on("create_room", () => {
+    const roomId = crypto.randomBytes(3).toString('hex')
+    // const link = `http://localhost:5173/join/${roomId}`
+    socket.emit('receive_link', roomId)
+  })
+})
 
 function listenForRequests() {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
+  const port = process.env.PORT || 3001;
+  server.listen(port, () => {
     console.log("Now listening on port", port);
   });
 }
