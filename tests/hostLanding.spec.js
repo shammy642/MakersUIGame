9ac09f9e759319195feb68b75b50fc18b1f494b7
Joe Winter <joewinter333@gmail.com>
2024-10-18 21:49:44 +0100

    landing host e2e

--- tests/hostLanding.spec.js
+++ tests/hostLanding.spec.js
@@ -1,10 +1,36 @@
+const { test, expect } = require("@playwright/test");
 
-const { test, expect } = require('@playwright/test');
+test.describe("Host landing page", () => {
+  test("has game title", async ({ page }) => {
+    await page.goto("/");
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+    const gameName = page.locator('[data-testid="game-name"]');
+    await expect(gameName).toHaveText("Guess the number!");
+  });
+  test("can enter username into field", async ({ page }) => {
+    await page.goto("/");
 
-  // Expect a title "to contain" a substring.
-  const gameName = page.locator('h1[data-testid="game-name"]');
-  await expect(gameName).toHaveText('Guess the number!');
+    const inputEl = page.getByPlaceholder("Username");
+    await inputEl.fill("Joe");
+
+    expect(inputEl).toHaveValue("Joe");
+  });
+
+  test("Create game button to exist", async ({ page }) => {
+    await page.goto("/");
+    const buttonEl = page.getByRole("button");
+
+    expect(buttonEl).toHaveText("Create Game");
+  });
+  test("inputing a name and then going to the next page", async ({ page }) => {
+    await page.goto("/");
+
+    const inputEl = page.getByPlaceholder("Username");
+    await inputEl.fill("Joe");
+
+    const buttonEl = page.getByRole("button");
+    await buttonEl.click();
+
+    await expect(page).toHaveURL("/lobby/host");
+  });
 });
