3cddf1d8ec63cceb42f0c55790546947afb5275a
Joe Winter <joewinter333@gmail.com>
2024-10-21 12:59:35 +0100

    updated list of components so testing is easier

--- frontend/tests/pages/InGame.test.jsx
+++ frontend/tests/pages/InGame.test.jsx
@@ -5,23 +5,35 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { InGame } from "../../src/pages/InGame";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { ListPlayers } from "../../src/components/ListPlayers";
 
-describe("InGame tests", ()  => {
-    test("name of the game", () => {
-        const players = [{
-            currentGuess : null,
-            id :"WHbOG6ET1uHeg-MqAAA8",
-            name : "Alexia(Host)", 
-            totalScore : 0
-        }]
-        render(
-            <BrowserRouter>
-                <InGame players={players}/>
-            </BrowserRouter>
-        );       
-        const heading = screen.getByTestId("guess-label");
-        expect(heading.textContent).toEqual("Guess a number between 1 and 100!");
-    });
+const setRedirect = vi.fn();
+
+const players = [
+  {
+    currentGuess: null,
+    id: "WHbOG6ET1uHeg-MqAAA8",
+    name: "Alexia(Host)",
+    totalScore: 0,
+  },
+  {
+    currentGuess: null,
+    id: "werjighig52-54252tgv",
+    name: "Lucy",
+    totalScore: 2,
+  },
+
+];
+
+describe("InGame tests", () => {
+  test("name of the game", () => {
+    render(
+      <BrowserRouter>
+        <InGame players={players} redirect={""} setRedirect={setRedirect} />
+      </BrowserRouter>
+    );
+    const heading = screen.getByTestId("guess-label");
+    expect(heading.textContent).toEqual("Guess a number between 1 and 100!");
+  });
 });
