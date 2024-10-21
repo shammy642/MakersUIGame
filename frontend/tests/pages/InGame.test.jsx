f51d65a901098162536021541ed752fd0e69d0de
Joe Winter <joewinter333@gmail.com>
2024-10-21 14:03:40 +0100

    added in game tests

--- frontend/tests/pages/InGame.test.jsx
+++ frontend/tests/pages/InGame.test.jsx
@@ -3,12 +3,24 @@
 
 //required imports
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { InGame } from "../../src/pages/InGame";
 import { describe, expect, test, vi } from "vitest";
-import { ListPlayers } from "../../src/components/ListPlayers";
+import userEvent from "@testing-library/user-event";
+import { act } from "react";
+import { socket } from "../../src/socket";
 
 const setRedirect = vi.fn();
+
+vi.mock("../../src/socket.js", () => {
+  return { socket: { emit: vi.fn() } };
+});
+
+vi.mock("react-router-dom", () => {
+  const navigateMock = vi.fn();
+  const useNavigateMock = () => navigateMock;
+  return { useNavigate: useNavigateMock };
+});
 
 const players = [
   {
@@ -23,17 +35,65 @@ const players = [
     name: "Lucy",
     totalScore: 2,
   },
-
 ];
 
 describe("InGame tests", () => {
   test("name of the game", () => {
     render(
-      <BrowserRouter>
         <InGame players={players} redirect={""} setRedirect={setRedirect} />
-      </BrowserRouter>
     );
     const heading = screen.getByTestId("guess-label");
     expect(heading.textContent).toEqual("Guess a number between 1 and 100!");
+  });
+  test("given a list of players, they are visible", () => {
+    render(
+        <InGame players={players} redirect={""} setRedirect={setRedirect} />
+    );
+    expect(screen.getByText("Alexia(Host)"));
+    expect(screen.getByText("Lucy"));
+  });
+  test("a user can input a number", async () => {
+    const user = userEvent.setup();
+    render(
+        <InGame players={players} redirect={""} setRedirect={setRedirect} />
+    );
+    const inputEl = screen.getByPlaceholderText("Your guess");
+    await act(async () => {
+      await user.type(inputEl, "10");
+    });
+    expect(screen.getAllByDisplayValue("10"));
+  });
+  test("a button to make guess exists", () => {
+    render(
+        <InGame players={players} redirect={""} setRedirect={setRedirect} />
+    );
+
+    const buttonEl = screen.getByRole("button", { name: "Guess" });
+    expect(buttonEl).toBeDefined();
+  });
+  test("when a user makes a guess, the socket emits", async () => {
+    const user = userEvent.setup();
+    render(
+        <InGame players={players} redirect={""} setRedirect={setRedirect} />
+    );
+    const inputEl = screen.getByPlaceholderText("Your guess");
+    await act(async () => {
+      await user.type(inputEl, "10");
+    });
+    const buttonEl = screen.getByRole("button", { name: "Guess" });
+    await user.click(buttonEl);
+    expect(socket.emit).toHaveBeenCalledWith("send_number", "10");
+  });
+  test("when the redirect state is changed the user is redirect", () => {
+    render(
+        <InGame
+          players={players}
+          redirect={"test_route"}
+          setRedirect={setRedirect}
+        />
+    );
+    const navigate = useNavigate();
+    expect(navigate).toHaveBeenCalledWith("test_route");
+    expect(setRedirect).toHaveBeenCalledWith("")
   });
 });
