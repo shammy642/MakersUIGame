9ce302cb0e925520d8f0c503efd138162f4454fd
Sam Hoile <samuel.hoile@gmail.com>
2024-10-22 13:15:41 +0100

    added gif to homepage

--- frontend/src/pages/LandingHost.jsx
+++ frontend/src/pages/LandingHost.jsx
@@ -17,13 +17,15 @@ export function LandingHost() {
   };
   return (
     <>
-      <div className="home">
-        <h1 data-testid="game-name">Guess the number!</h1>
-        <p>Rules of the game:</p>
-        <ul>
+      <div className="home flex-col justify-items-center">
+        <h1 data-testid="game-name" className="text-xl mb-1 font-bold">Guess the Pokemon&apos;s weight!</h1>
+        <p>A quick-fire multiplayer game</p>
+        
+        <img src="https://i.gifer.com/5SvD.gif" className="max-w-20"/>
+        {/*<ul>
           <li>Guess the number between 1 and 100</li>
           <li>2 to 6 players</li>
-        </ul>
+        </ul> */}
         <Form input={input} setInput={setInput}></Form>
         <br></br>
         <Button handleClick={handleClick} buttonText="Create Game"></Button>
