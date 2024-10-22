1227d80601237f197354a5661377659ea90d856a
Lucia_F <luciafernandez4e@gmail.com>
2024-10-22 12:22:01 +0100

    Removed unused component

--- frontend/src/components/Username.jsx
+++
@@ -1,9 +0,0 @@
-//A textbox that will allow a user to input text as a username or number
-export function Username(props) {
-
-    return (
-      <>
-      <button onClick={props.handleClick} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{props.buttonText}</button>
-      </>
-    )
-  }
