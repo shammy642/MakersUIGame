d643e7e31614b45099bd59da12d7dbebc4468bbf
Sam Hoile <samuel.hoile@gmail.com>
2024-10-23 16:29:22 +0100

    add min-size to card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,6 +1,6 @@
 export function Card({ children }) {
     return (
-        <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="min-w-96 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             { children }
         </div>
     )
