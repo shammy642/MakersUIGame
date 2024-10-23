e04e8f4f59c316aeba44c6c2a1da03d1ad9ec850
James Harker <james.harker@hotmail.co.uk>
2024-10-23 11:12:01 +0100

    cards-on-pages-grrrrr

--- frontend/src/pages/RoundEnd.jsx
+++ frontend/src/pages/RoundEnd.jsx
@@ -5,6 +5,7 @@ import { Button } from "../components/Button";
 //import { ExitButton } from "../components/ExitButton"
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { Card } from "../components/Card";
 
 export function RoundEnd({ gameState, redirect, setRedirect, pokemon }) {
   const navigate = useNavigate();
@@ -27,50 +28,50 @@ export function RoundEnd({ gameState, redirect, setRedirect, pokemon }) {
 
   return (
     <div className="full-page">
-    <div className="round-end">
-      <div className="m-3">
-        <h2 className="text-2xl">Scores</h2>
-        <ol>
-          {gameState &&
-            gameState.players &&
-            gameState.players.map((player, index) => (
-              <p key={`${player.name}-${index}`}>
-                {player.name}: {player.totalScore}
-              </p>
-            ))}
-        </ol>
-      </div>
+      <Card>
+        <div className="m-3">
+          <h2 className="text-2xl">Scores</h2>
+          <ol>
+            {gameState &&
+              gameState.players &&
+              gameState.players.map((player, index) => (
+                <p key={`${player.name}-${index}`}>
+                  {player.name}: {player.totalScore}
+                </p>
+              ))}
+          </ol>
+        </div>
 
-      <div className="m-3">
-        <p data-testid="number-reveal">
-          <b>{pokemon.name}</b>&apos;s weight is
-        </p>
-        <h2 className="text-5xl">{pokemon.weight}</h2>
-        <h2 className="font-bold">Hectograms</h2>
-      </div>
-      <div className="flex justify-center">
-        <img src={pokemon.pictureURL} />
-      </div>
-      <div className="m-3">
-        {(gameState &&
+        <div className="m-3">
+          <p data-testid="number-reveal">
+            <b>{pokemon.name}</b>&apos;s weight is
+          </p>
+          <h2 className="text-5xl">{pokemon.weight}</h2>
+          <h2 className="font-bold">Hectograms</h2>
+        </div>
+        <div className="flex justify-center">
+          <img src={pokemon.pictureURL} />
+        </div>
+        <div className="m-3">
+          {(gameState &&
             gameState.currentRoundWinner && gameState.currentRoundWinner.name !== "") ? <><p>The winner is...</p>
-        <h2 className="text-4xl">
-          {gameState &&
-            gameState.currentRoundWinner &&
-            gameState.currentRoundWinner.name}
-        </h2>
-        </>
-        :
-        <><h2 className="text-xl">No one guessed in time!</h2>
-        <p>Poor {pokemon.name}...</p></>
-        }
-      </div>
+            <h2 className="text-4xl">
+              {gameState &&
+                gameState.currentRoundWinner &&
+                gameState.currentRoundWinner.name}
+            </h2>
+          </>
+            :
+            <><h2 className="text-xl">No one guessed in time!</h2>
+              <p>Poor {pokemon.name}...</p></>
+          }
+        </div>
 
-      <div>
-        <Button handleClick={handleNextRound} buttonText={"Next Round"} />
-        <Button handleClick={handleQuitGame} buttonText={"Quit Game"} />
-      </div>
-    </div>
+        <div>
+          <Button handleClick={handleNextRound} buttonText={"Next Round"} />
+          <Button handleClick={handleQuitGame} buttonText={"Quit Game"} />
+        </div>
+      </Card>
     </div>
   );
 }
