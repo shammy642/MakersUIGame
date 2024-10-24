cc9cdb5e445457c8059fe25361590213254db4a7
Sam Hoile <samuel.hoile@gmail.com>
2024-10-24 16:54:09 +0100

    fix width

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,7 +1,7 @@
 export function Card({ children }) {
-    return (
-        <div className="block max-w-lg min-w-120 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 font-normal text-gray-700 dark:text-gray-400">
-                { children }
-        </div>
-    )
+  return (
+    <div className="block w-96 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 font-normal text-gray-700 dark:text-gray-400">
+      {children}
+    </div>
+  );
 }
