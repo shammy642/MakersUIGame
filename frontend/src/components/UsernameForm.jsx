6696c0d896bfc7c05ddd74b0af21f4c2cf742709
Lucia_F <luciafernandez4e@gmail.com>
2024-10-24 14:09:18 +0100

    fixed edit icon so it stays on at all times

--- frontend/src/components/UsernameForm.jsx
+++ frontend/src/components/UsernameForm.jsx
@@ -54,7 +54,7 @@ export function UsernameForm({ input, setInput, avatar, setAvatar, error }) {
                   aria-label="default avatar image"
                   className="w-8 h-8"
                 ></img>
-                {/*  <svg
+                <svg
                   className="w-6 h-6 text-gray-500 dark:text-gray-400"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
@@ -62,7 +62,7 @@ export function UsernameForm({ input, setInput, avatar, setAvatar, error }) {
                   viewBox="0 0 20 20"
                 >
                   <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-                </svg> */}
+                </svg>
               </>
             )}
           </span>
