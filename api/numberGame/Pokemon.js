d32882af7146b46de8d35d5ec9abc584326272b7
Sam Hoile <samuel.hoile@gmail.com>
2024-10-24 10:55:59 +0100

    added minPokemon and retry logic

--- api/numberGame/Pokemon.js
+++ api/numberGame/Pokemon.js
@@ -1,14 +1,20 @@
 class Pokemon {
   constructor() {
+    this._minPokemon = 1
     this._maxPokemon = 1025;
   }
 
   async getRandom() {
-    const id = Math.floor(Math.random() * this._maxPokemon);
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const pokemon = await response.json()
-    return this._extractInfo(pokemon)
-
+    const id = Math.floor(Math.random() * (this._maxPokemon - this._minPokemon + 1)) + this._minPokemon;
+    
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      const pokemon = await response.json()
+      return this._extractInfo(pokemon)
+    } catch(error) {
+      console.log("Error fetching pokemon, re-fetching!", error)
+      return this.getRandom()
+    }
   }
 
   _extractInfo(pokemon) {
